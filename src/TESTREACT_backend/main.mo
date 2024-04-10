1949b825eb484accf554305b7c8793acd722fde5
ASCM5 <utm22030590@utma.edu.mx>
2024-04-09 21:27:11 -0600

    Adicion de boton para inicio de secion con Internet Identity

--- src/TESTREACT_backend/main.mo
+++ src/TESTREACT_backend/main.mo
@@ -1,5 +1,9 @@
+import Principal "mo:base/Principal";
+
+
+
 actor {
-  public query func greet(name : Text) : async Text {
-    return "Hello, " # name # "!";
+  public query (message) func greet() : async Text {
+    return "Hello, " # Principal.toText(message.caller) # "!";
   };
 };
--- src/TESTREACT_frontend/src/App.jsx
+++ src/TESTREACT_frontend/src/App.jsx
@@ -1,5 +1,54 @@
 import { useState } from 'react';
-import { TESTREACT_backend } from 'declarations/TESTREACT_backend';
+import { createActor, TESTREACT_backend } from '../../declarations/TESTREACT_backend';
+import {AuthClient} from "@dfinity/auth-client"
+import {HttpAgent} from "@dfinity/agent";
+
+let actor = TESTREACT_backend;
+
+//conexion a INTERNET IDENTITY
+const greetButton = document.getElementById("greet");
+greetButton.onclick = async (e) => {
+    e.preventDefault();
+
+    greetButton.setAttribute("disabled", true);
+
+    // Interact with backend actor, calling the greet method
+    const greeting = await actor.greet();
+
+    greetButton.removeAttribute("disabled");
+
+    document.getElementById("greeting").innerText = greeting;
+
+    return false;
+};
+
+const loginButton = document.getElementById("login");
+loginButton.onclick = async (e) => {
+    e.preventDefault();
+
+    // create an auth client
+    let authClient = await AuthClient.create();
+
+    // start the login process and wait for it to finish
+    await new Promise((resolve) => {
+        authClient.login({
+            identityProvider: process.env.II_URL,
+            onSuccess: resolve,
+        });
+    });
+
+    // At this point we're authenticated, and we can get the identity from the auth client:
+    const identity = authClient.getIdentity();
+    // Using the identity obtained from the auth client, we can create an agent to interact with the IC.
+    const agent = new HttpAgent({identity});
+    // Using the interface description of our webapp, we create an actor that we use to call the service methods.
+    actor = createActor(process.env.GREET_BACKEND_CANISTER_ID, {
+        agent,
+    });
+
+    return false;
+};
+//FIN DE FUNCIONES PARA CONEXION
 
 function App() {
   const [greeting, setGreeting] = useState('');
@@ -15,13 +64,11 @@ function App() {
 
   return (
     <main>
-      <img src="/logo2.svg" alt="DFINITY logo" />
-      <br />
-      <br />
+
       <form action="#" onSubmit={handleSubmit}>
-        <label htmlFor="name">Enter your name: &nbsp;</label>
+        <label htmlFor="name">Escribe algo: &nbsp;</label>
         <input id="name" alt="Name" type="text" />
-        <button type="submit">Click Me!</button>
+        <button type="submit">Enviar</button>
       </form>
       <section id="greeting">{greeting}</section>
     </main>
